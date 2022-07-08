cbb2c71e5c04097522b3ac4a6d151d27417c337e
CIANDT\pietros <pietros@ciandt.com>
2022-07-08 15:28:09 -0300

    jpa_h2_demo

---
+++ 0x04/jpa-h2-demo/com/example/jpah2demo/ClienteController.java
@@ -0,0 +1,77 @@
+package example.jpah2demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping("/addClient")
+    public ResponseEntity<Cliente> addClient(@RequestBody Cliente cliente) {
+        try{
+            return new ResponseEntity<>(clienteRepository.save(cliente), HttpStatus.CREATED);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/findAllClients")
+    public ResponseEntity<List<Cliente>> findAllClients() {
+        try {
+            return new ResponseEntity(clienteRepository.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/findClientById/{id}")
+    public ResponseEntity<Cliente> findClientById(@PathVariable("id") Long idClient) {
+        Optional<Cliente> cliente = this.clienteRepository.findById(idClient);
+
+        if (cliente.isPresent()) return new ResponseEntity<Cliente>(cliente.get(), HttpStatus.OK);
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/removeClientById/{id}")
+    public ResponseEntity<HttpStatus> removerCliente(@PathVariable("id") Long idClient) {
+        try {
+            this.clienteRepository.deleteById(idClient);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/updateClientById/{id}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable("id") Long id, @RequestBody Cliente cliente) {
+        Optional<Cliente> clienteOld = this.clienteRepository.findById(id);
+
+        if (clienteOld.isPresent()) {
+            Cliente clienteUpdated = clienteOld.get();
+            clienteUpdated.setNome(cliente.getNome());
+            clienteUpdated.setIdade(cliente.getIdade());
+            clienteUpdated.setEmail(cliente.getEmail());
+            clienteUpdated.setTelefones(cliente.getTelefones());
+            clienteUpdated.setEnderecos(cliente.getEnderecos());
+
+            return new ResponseEntity<>(this.clienteRepository.save(clienteUpdated), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+ }
+
+
+
---
+++ 0x04/jpa-h2-demo/com/example/jpah2demo/Telefone.java
@@ -0,0 +1,55 @@
+package example.jpah2demo;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "telefone")
+public class Telefone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String ddd;
+
+    @Column
+    private String numero;
+
+    @ManyToOne
+    private Cliente cliente;
+
+    public Telefone(){
+    }
+
+    public Telefone(String ddd, String numero, Cliente cliente) {
+        this.ddd = ddd;
+        this.numero = numero;
+        this.cliente = cliente;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
