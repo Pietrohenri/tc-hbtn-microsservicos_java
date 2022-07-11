48034c0479996d930f502bf1476e4b3a25a446e7
CIANDT\pietros <pietros@ciandt.com>
2022-07-11 13:38:21 -0300

    calculatorAPI

--- 0x07/api-rest-calculator/src/main/java/com/example/calculator/CalculatorApiApplication.java
+++ 0x07/api-rest-calculator/src/main/java/com/example/calculator/CalculatorApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRestCalculatorApplication {
+public class CalculatorApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiRestCalculatorApplication.class, args);
+		SpringApplication.run(com.example.calculator.CalculatorApiApplication.class, args);
 	}
 
 }
